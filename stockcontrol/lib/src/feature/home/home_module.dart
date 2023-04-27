0d1281355d0a7a0d1fc43f00b3aaca43cac415b4
gregor_yuri <gregoryuri09@gmail.com>
2023-04-27 00:59:41 -0300

    arrumando importações

--- stockcontrol/lib/src/feature/home/home_module.dart
+++ stockcontrol/lib/src/feature/home/home_module.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:stockcontrol/src/feature/home/view/page/singuppage.dart';
+import 'package:stock_control/src/feature/home/view/page/singuppage.dart';
 import '../home/view/page/homepage.dart';
 import '../home/viewmodel/home_viewmodel.dart';
 import '../home/view/page/loginpage.dart';
--- stockcontrol/lib/src/feature/home/view/page/singuppage.dart
+++ stockcontrol/lib/src/feature/home/view/page/singuppage.dart
@@ -1,10 +1,9 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:localization/localization.dart';
-import 'package:stockcontrol/src/feature/home/view/page/loginpage.dart';
-
+import 'package:stock_control/src/feature/home/view/page/loginpage.dart';
 import 'homepage.dart';
+import 'loginpage.dart';
 
 class SignupPage extends StatefulWidget {
   @override
