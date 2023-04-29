909858c4d3e5b869a0866d566c0d6568872af906
OfujiDEV <ofujidev@gmail.com>
2023-04-29 17:54:57 -0300

    Verifica Validação LoginPage

--- stockcontrol/lib/src/feature/home/view/page/loginpage.dart
+++ stockcontrol/lib/src/feature/home/view/page/loginpage.dart
@@ -4,6 +4,8 @@ import 'package:stock_control/src/feature/home/view/page/homepage.dart';
 import 'package:stock_control/src/feature/home/view/page/singuppage.dart';
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -14,6 +16,10 @@ class _LoginPageState extends State<LoginPage> {
   // ignore: unused_field
   late String _password;
 
+  final _emailController = TextEditingController();
+  final _senhaController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,61 +27,82 @@ class _LoginPageState extends State<LoginPage> {
       body: Center(
         child: Padding(
           padding: EdgeInsets.all(16.0),
-          child: ListView(
-            children: [
-              Text(
-                "acessar".i18n(),
-                style: const TextStyle(
-                    fontSize: 40, color: Color.fromARGB(255, 16, 52, 153)),
-              ),
-              SizedBox(height: 90),
-              TextFormField(
-                decoration: InputDecoration(labelText: "email".i18n()),
-                onChanged: (value) {
-                  setState(() {
-                    _email = value;
-                  });
-                },
-              ),
-              SizedBox(height: 30),
-              TextFormField(
-                decoration: InputDecoration(labelText: "senha".i18n()),
-                obscureText: true,
-                onChanged: (value) {
-                  setState(() {
-                    _password = value;
-                  });
-                },
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SignupPage()));
-                },
-                icon: Text(
-                  "esqueci_a_senha".i18n(),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                Text(
+                  "acessar".i18n(),
+                  style: const TextStyle(
+                      fontSize: 40, color: Color.fromARGB(255, 16, 52, 153)),
                 ),
-              ),
-              SizedBox(height: 150),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const HomePage()));
-                },
-                child: Text("entrar".i18n()),
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SignupPage()));
-                },
-                icon: Text(
-                  "nao_tem_conta_cadastre".i18n(),
+                SizedBox(height: 90),
+                TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(labelText: "email".i18n()),
+                    validator: (_email) {
+                      if (_email == null || _email.isEmpty) {
+                        return 'Digite seu e-mail';
+                      } else
+                        onChanged:
+                        (value) {
+                          setState(
+                            () {
+                              _email = value;
+                            },
+                          );
+                        };
+                    }),
+                SizedBox(height: 30),
+                TextFormField(
+                    controller: _senhaController,
+                    decoration: InputDecoration(labelText: "senha".i18n()),
+                    obscureText: true,
+                    validator: (_password) {
+                      if (_password == null || _password.isEmpty) {
+                        return 'Digite sua senha';
+                      } else
+                        onChanged:
+                        (value) {
+                          setState(
+                            () {
+                              _password = value;
+                            },
+                          );
+                        };
+                    }),
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SignupPage()));
+                  },
+                  icon: Text(
+                    "esqueci_a_senha".i18n(),
+                  ),
                 ),
-              )
-            ],
+                SizedBox(height: 150),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomePage()));
+                    }
+                  },
+                  child: Text("entrar".i18n()),
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SignupPage()));
+                  },
+                  icon: Text(
+                    "nao_tem_conta_cadastre".i18n(),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- stockcontrol/lib/src/main.dart
+++ stockcontrol/lib/src/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:stock_control/services/auth_services.dart';
 
 import 'app_module.dart';
 import 'app_widget.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
+    FirebaseFirestore.instance.collection('teste').add({'teste':'teste'});
+    
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
