90b14c294666217d3ec36235c2c91d846f364bac
OfujiDEV <ofujidev@gmail.com>
2023-04-29 12:04:44 -0300

    Branch teste Firebase

--- stockcontrol/lib/src/main.dart
+++ stockcontrol/lib/src/main.dart
@@ -4,7 +4,14 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'app_module.dart';
 import 'app_widget.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ModularApp(module: AppModule(), child: const AppWidget()),
   );
