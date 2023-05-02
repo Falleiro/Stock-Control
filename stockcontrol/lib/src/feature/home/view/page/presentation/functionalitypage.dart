7c6a9b42477124ef9ca64d7b910f390f9542322b
gregor_yuri <gregoryuri09@gmail.com>
2023-05-02 00:54:49 -0300

    finalizando telas de apresentação

--- stockcontrol/lib/src/feature/home/view/page/presentation/functionalitypage.dart
+++ stockcontrol/lib/src/feature/home/view/page/presentation/functionalitypage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:localization/localization.dart';
-
 import '../login/loginpage.dart';
+import 'objectivepage.dart';
 
 class FuncionalidadesScreen extends StatelessWidget {
   const FuncionalidadesScreen({super.key});
@@ -15,40 +15,71 @@ class FuncionalidadesScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'Funcionalidades do Aplicativo',
+            SizedBox(height: 40.0),
+            Text(
+              "funcionalidade_app".i18n(),
               style: const TextStyle(
-                  fontSize: 40, color: Color.fromARGB(255, 16, 52, 153)),
+                  fontSize: 36, color: Color.fromARGB(255, 16, 52, 153)),
             ),
             SizedBox(height: 16.0),
             Expanded(
               child: ListView(
-                children: const [
+                children: [
                   ListTile(
-                    title: Text('Cadastro de produtos'),
-                    subtitle: Text(
-                        'Adicione produtos ao estoque e mantenha um registro atualizado.'),
+                    title: Text("cadastro_produto".i18n()),
+                    subtitle: Text("texto_cadastro".i18n()),
                   ),
                   ListTile(
-                    title: Text('Controle de estoque'),
-                    subtitle: Text(
-                        'Acompanhe o estoque dos seus produtos em tempo real.'),
+                    title: Text("controle_estoque".i18n()),
+                    subtitle: Text("texto_controle".i18n()),
                   ),
                   ListTile(
-                    title: Text('Gestão de franquias'),
-                    subtitle: Text(
-                        'Gerencie suas lojas em diferentes locais e acompanhe as vendas de cada uma.'),
+                    title: Text("gestao_estabelecimento".i18n()),
+                    subtitle: Text("gestao_texto".i18n()),
+                  ),
+                  ListTile(
+                    title: Text("sugestao_produto".i18n()),
+                    subtitle: Text("texto_sugestao".i18n()),
                   ),
                 ],
               ),
             ),
-            const SizedBox(height: 32.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => LoginPage()));
-              },
-              child: Text("finalizar".i18n()),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0, left: 16.0),
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ObjectivesPage()));
+                      },
+                      child: Text("voltar".i18n()),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0, right: 16.0),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginPage(),
+                          ),
+                        );
+                      },
+                      child: Text("finalizar".i18n()),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
--- stockcontrol/lib/src/feature/home/view/page/presentation/presentationpage.dart
+++ stockcontrol/lib/src/feature/home/view/page/presentation/presentationpage.dart
@@ -1,50 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:localization/localization.dart';
-
 import 'objectivepage.dart';
 
 // ignore: camel_case_types
-class presentationpage extends StatefulWidget {
-  const presentationpage({super.key});
+class PresentationPage extends StatefulWidget {
+  const PresentationPage({Key? key}) : super(key: key);
 
   @override
   // ignore: library_private_types_in_public_api
   _PresentationPageState createState() => _PresentationPageState();
 }
 
-class _PresentationPageState extends State<presentationpage> {
+class _PresentationPageState extends State<PresentationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(248, 231, 231, 231),
+      backgroundColor: const Color.fromARGB(248, 231, 231, 231),
       body: Container(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Image.asset('assets/images/logo.png', height: 120), precisa bolar uma logo
-            const SizedBox(height: 16.0),
+            const SizedBox(height: 30.0),
             Text(
               "app_apresentação".i18n(),
               style: const TextStyle(
-                  fontSize: 40, color: Color.fromARGB(255, 16, 52, 153)),
+                  fontSize: 30, color: Color.fromARGB(255, 16, 52, 153)),
             ),
-
-            const SizedBox(height: 16.0),
+            const Icon(Icons.store,
+                size: 80, color: Color.fromARGB(255, 16, 52, 153)),
+            const SizedBox(height: 100.0),
+            Text(
+              "bem_vindo".i18n(),
+              style: const TextStyle(fontSize: 24.0),
+              textAlign: TextAlign.left,
+            ),
             Text(
               "texto_apresentação".i18n(),
-              style: const TextStyle(fontSize: 16.0),
-              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 22.0),
+              textAlign: TextAlign.justify,
             ),
-            const SizedBox(height: 32.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ObjectivesScreen()));
-              },
-              child: Text("proximo".i18n()),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0, right: 16.0),
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ObjectivesPage(),
+                      ),
+                    );
+                  },
+                  child: Text("proximo".i18n()),
+                ),
+              ),
             ),
           ],
         ),
