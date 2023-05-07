8f919819799c3f96b4a6887b93fbffcdee9572c8
gregor_yuri <gregoryuri09@gmail.com>
2023-05-07 13:44:42 -0300

    impedindo que o usuario volte para a tela de login da tela inicial

--- stockcontrol/lib/src/feature/home/view/page/login/singuppage.dart
+++ stockcontrol/lib/src/feature/home/view/page/login/singuppage.dart
@@ -82,109 +82,114 @@ class _SignupPageState extends State<SignupPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(248, 231, 231, 231),
-      body: Center(
-        child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ListView(
-              children: [
-                Text(
-                  "criar_nova_conta".i18n(),
-                  style: const TextStyle(
-                      fontSize: 30, color: Color.fromARGB(255, 16, 52, 153)),
-                ),
-                SizedBox(height: 30),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 30),
-                    TextFormField(
-                      decoration: InputDecoration(labelText: "nome".i18n()),
-                      onChanged: (value) {
-                        setState(() {
-                          _name = value;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 30),
-                    TextFormField(
-                      decoration: InputDecoration(labelText: "email".i18n()),
-                      onChanged: (value) {
-                        setState(() {
-                          _email = value;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 30),
-                    TextFormField(
-                      decoration:
-                          InputDecoration(labelText: "data_nascimento".i18n()),
-                      onChanged: (value) {
-                        setState(() {
-                          _birthdate = value;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 30),
-                    TextFormField(
-                      decoration: InputDecoration(labelText: "senha".i18n()),
-                      obscureText: true,
-                      onChanged: (value) {
-                        setState(() {
-                          _password = value;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 30),
-                    TextFormField(
-                      decoration:
-                          InputDecoration(labelText: "repita_senha".i18n()),
-                      obscureText: true,
-                      onChanged: (value) {
-                        setState(() {
-                          _confirmPassword = value;
-                        });
-                      },
-                    ),
-                    const SizedBox(height: 60.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (checkPasswordsMatch(_password, _confirmPassword)) {
-                          creatuser(_email, _password);
-                        } else {
-                          showDialog(
-                              context: context,
-                              builder: (context) => AlertDialog(
-                                    title: Text("erro".i18n()),
-                                    content: Text("senha_diferente".i18n()),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () => Navigator.pop(context),
-                                        child: Text("ok".i18n()),
-                                      ),
-                                    ],
-                                  ));
-                        }
-                      },
-                      child: Text("cadastrar".i18n()),
-                    ),
-                  ],
-                ),
-                IconButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => LoginPage()),
-                    );
-                  },
-                  iconSize: 50,
-                  icon: Text(
-                    "ja_tem_conta_acesse_aqui".i18n(),
+    return WillPopScope(
+      onWillPop: () async => false, // Impede o usuário de voltar
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(248, 231, 231, 231),
+        body: Center(
+          child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListView(
+                children: [
+                  Text(
+                    "criar_nova_conta".i18n(),
+                    style: const TextStyle(
+                        fontSize: 30, color: Color.fromARGB(255, 16, 52, 153)),
                   ),
-                )
-              ],
-            )),
+                  SizedBox(height: 30),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 30),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "nome".i18n()),
+                        onChanged: (value) {
+                          setState(() {
+                            _name = value;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 30),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "email".i18n()),
+                        onChanged: (value) {
+                          setState(() {
+                            _email = value;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 30),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: "data_nascimento".i18n()),
+                        onChanged: (value) {
+                          setState(() {
+                            _birthdate = value;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 30),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: "senha".i18n()),
+                        obscureText: true,
+                        onChanged: (value) {
+                          setState(() {
+                            _password = value;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 30),
+                      TextFormField(
+                        decoration:
+                            InputDecoration(labelText: "repita_senha".i18n()),
+                        obscureText: true,
+                        onChanged: (value) {
+                          setState(() {
+                            _confirmPassword = value;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 60.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (checkPasswordsMatch(
+                              _password, _confirmPassword)) {
+                            creatuser(_email, _password);
+                          } else {
+                            showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                      title: Text("erro".i18n()),
+                                      content: Text("senha_diferente".i18n()),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () =>
+                                              Navigator.pop(context),
+                                          child: Text("ok".i18n()),
+                                        ),
+                                      ],
+                                    ));
+                          }
+                        },
+                        child: Text("cadastrar".i18n()),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    },
+                    iconSize: 50,
+                    icon: Text(
+                      "ja_tem_conta_acesse_aqui".i18n(),
+                    ),
+                  )
+                ],
+              )),
+        ),
       ),
     );
   }
