65cd7b9444ea5ab14207bf9067175e97b32c279b
gregor_yuri <gregoryuri09@gmail.com>
2023-06-15 18:48:45 -0300

    Iniciando para colocar nome e data, e editá-los

--- stockcontrol/lib/src/feature/pages/login/singuppage.dart
+++ stockcontrol/lib/src/feature/pages/login/singuppage.dart
@@ -1,14 +1,12 @@
-// ignore_for_file: unused_field
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:localization/localization.dart';
 import '../homepage/homepage.dart';
 import 'loginpage.dart';
 import 'package:stock_control/src/services/firebase_auth_service.dart';
 
 class SignupPage extends StatefulWidget {
-  const SignupPage({super.key});
-
   @override
   _SignupPageState createState() => _SignupPageState();
 }
@@ -28,40 +26,41 @@ class _SignupPageState extends State<SignupPage> {
     return password == confirmPassword;
   }
 
-  // ignore: non_constant_identifier_names
-  creatuser(String emailrec, String passwordrec) async {
-    if (emailrec.isEmpty || passwordrec.isEmpty) {
+  Future<void> createUser(String email, String password) async {
+    if (email.isEmpty || password.isEmpty) {
       showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-                title: Text("erro".i18n()),
-                content: Text("email_senha_origatorio".i18n()),
-                actions: [
-                  TextButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: Text("ok".i18n()),
-                  ),
-                ],
-              ));
-    }
-    try {
-      final userCredential =
-          await FirebaseAuthService().createUserWithEmailAndPassword(
-        email: emailrec,
-        password: passwordrec,
-      );
-      debugPrint('Usuário criado com sucesso: ${userCredential.user!.email}');
-      final currentContext = context;
-      Future.delayed(
-        Duration.zero,
-        () {
-          Navigator.push(
-            currentContext,
-            MaterialPageRoute(
-              builder: (context) => const HomePage(),
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("erro".i18n()),
+          content: Text("email_senha_obrigatorio".i18n()),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text("ok".i18n()),
             ),
-          );
-        },
+          ],
+        ),
+      );
+      return;
+    }
+
+    try {
+      final userCredential = await FirebaseAuthService()
+          .createUserWithEmailAndPassword(email: email, password: password);
+      debugPrint('Usuário criado com sucesso: ${userCredential.user!.email}');
+
+      // Salvar nome e data de nascimento no Realtime Database
+      final database = FirebaseDatabase.instance;
+      final userRef =
+          database.reference().child('users').child(userCredential.user!.uid);
+      await userRef.set({
+        'name': _name,
+        'birthdate': _birthdate,
+      });
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
       );
     } catch (e) {
       debugPrint('Erro ao criar usuário: $e');
@@ -100,136 +99,148 @@ class _SignupPageState extends State<SignupPage> {
     return WillPopScope(
       onWillPop: () async => false, // Impede o usuário de voltar
       child: Scaffold(
-        backgroundColor: const Color.fromARGB(248, 231, 231, 231),
+        backgroundColor: Color.fromARGB(248, 231, 231, 231),
         body: Center(
           child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ListView(
-                children: [
-                  Text(
-                    "criar_nova_conta".i18n(),
-                    style: const TextStyle(
-                        fontSize: 30, color: Color.fromARGB(255, 16, 52, 153)),
+            padding: const EdgeInsets.all(16.0),
+            child: ListView(
+              children: [
+                Text(
+                  "criar_nova_conta".i18n(),
+                  style: const TextStyle(
+                    fontSize: 30,
+                    color: Color.fromARGB(255, 16, 52, 153),
                   ),
-                  const SizedBox(height: 30),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 30),
-                      TextFormField(
-                        decoration: InputDecoration(labelText: "nome".i18n()),
-                        onChanged: (value) {
-                          setState(() {
-                            _name = value;
-                          });
-                        },
-                      ),
-                      const SizedBox(height: 30),
-                      TextFormField(
-                        decoration: InputDecoration(labelText: "email".i18n()),
-                        onChanged: (value) {
-                          setState(() {
-                            _email = value;
-                          });
-                        },
-                      ),
-                      const SizedBox(height: 30),
-                      TextFormField(
-                        decoration: InputDecoration(
-                            labelText: "data_nascimento".i18n()),
-                        onChanged: (value) {
-                          setState(() {
-                            _birthdate = value;
-                          });
-                        },
-                      ),
-                      const SizedBox(height: 30),
-                      TextFormField(
-                        decoration: InputDecoration(labelText: "senha".i18n()),
-                        obscureText: true,
-                        onChanged: (value) {
-                          setState(() {
-                            _password = value;
-                            validatePassword(value);
-                          });
-                        },
-                      ),
-                      const SizedBox(height: 30),
-                      Text(
-                        ' Pelo menos 6 caracteres',
-                        style: TextStyle(
-                            color: _isLengthValid ? Colors.green : Colors.red),
-                      ),
-                      Text(
-                        ' Pelo menos uma letra maiúscula',
-                        style: TextStyle(
-                            color:
-                                _isUpperCaseValid ? Colors.green : Colors.red),
-                      ),
-                      Text(
-                        ' Pelo menos um número',
-                        style: TextStyle(
-                            color: _isNumberValid ? Colors.green : Colors.red),
-                      ),
-                      Text(
-                        'Pelo menos um caractere especial',
-                        style: TextStyle(
-                            color: _isSpecialCharValid
-                                ? Colors.green
-                                : Colors.red),
-                      ),
-                      SizedBox(height: 30),
-                      TextFormField(
-                        decoration:
-                            InputDecoration(labelText: "repita_senha".i18n()),
-                        obscureText: true,
-                        onChanged: (value) {
-                          setState(() {
-                            _confirmPassword = value;
-                          });
-                        },
-                      ),
-                      const SizedBox(height: 60.0),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (checkPasswordsMatch(
-                              _password, _confirmPassword)) {
-                            creatuser(_email, _password);
-                          } else {
-                            showDialog(
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                      title: Text("erro".i18n()),
-                                      content: Text("senha_diferente".i18n()),
-                                      actions: [
-                                        TextButton(
-                                          onPressed: () =>
-                                              Navigator.pop(context),
-                                          child: Text("ok".i18n()),
-                                        ),
-                                      ],
-                                    ));
-                          }
-                        },
-                        child: Text("cadastrar".i18n()),
-                      ),
-                    ],
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginPage()),
-                      );
-                    },
-                    iconSize: 50,
-                    icon: Text(
-                      "ja_tem_conta_acesse_aqui".i18n(),
+                ),
+                SizedBox(height: 30),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 30),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: "nome".i18n()),
+                      onChanged: (value) {
+                        setState(() {
+                          _name = value;
+                        });
+                      },
                     ),
-                  )
-                ],
-              )),
+                    SizedBox(height: 30),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: "email".i18n()),
+                      onChanged: (value) {
+                        setState(() {
+                          _email = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 30),
+                    TextFormField(
+                      decoration:
+                          InputDecoration(labelText: "data_nascimento".i18n()),
+                      onChanged: (value) {
+                        setState(() {
+                          _birthdate = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 30),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: "senha".i18n()),
+                      obscureText: true,
+                      onChanged: (value) {
+                        setState(() {
+                          _password = value;
+                          validatePassword(value);
+                        });
+                      },
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Pelo menos 6 caracteres',
+                            style: TextStyle(
+                              color: _isLengthValid ? Colors.green : Colors.red,
+                            ),
+                          ),
+                          Text(
+                            'Pelo menos uma letra maiúscula',
+                            style: TextStyle(
+                              color:
+                                  _isUpperCaseValid ? Colors.green : Colors.red,
+                            ),
+                          ),
+                          Text(
+                            'Pelo menos um número',
+                            style: TextStyle(
+                              color: _isNumberValid ? Colors.green : Colors.red,
+                            ),
+                          ),
+                          Text(
+                            'Pelo menos um caractere especial',
+                            style: TextStyle(
+                              color: _isSpecialCharValid
+                                  ? Colors.green
+                                  : Colors.red,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    TextFormField(
+                      decoration:
+                          InputDecoration(labelText: "repita_senha".i18n()),
+                      obscureText: true,
+                      onChanged: (value) {
+                        setState(() {
+                          _confirmPassword = value;
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 60.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (checkPasswordsMatch(_password, _confirmPassword)) {
+                          createUser(_email, _password);
+                        } else {
+                          showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              title: Text("erro".i18n()),
+                              content: Text("senha_diferente".i18n()),
+                              actions: [
+                                TextButton(
+                                  onPressed: () => Navigator.pop(context),
+                                  child: Text("ok".i18n()),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
+                      },
+                      child: Text("cadastrar".i18n()),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  iconSize: 50,
+                  icon: Text(
+                    "ja_tem_conta_acesse_aqui".i18n(),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
