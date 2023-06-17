a9bb1b1e987bf0a842c164ec0079e01d5a0b06f7
gregor_yuri <61788444+gregor-21@users.noreply.github.com>
2023-06-17 16:14:10 -0300

    Revert "Wagna deselvolvimento"

--- README1.md
+++
@@ -1,2 +0,0 @@
-# pdm
-Repositório para projetos da matéria de Programação para Dispositivos Móveis
---
+++ stockcontrol/lib/src/feature/pages/item/item_edit.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:localization/localization.dart';
+import 'package:stock_control/src/feature/pages/item/widgets/delete_button.dart';
+import 'package:stock_control/src/feature/pages/item/widgets/row_form.dart';
+import 'package:stock_control/src/feature/repository/dao/itens_dao.dart';
+
+import '../../../component/my_appbar.dart';
+
+class UserItemEdit extends StatefulWidget {
+  final String name;
+  final int idEstabelecimento;
+  final int idItem;
+  final int qtdItem;
+  final String validade;
+  final int lote;
+  final Function(int) atualizaQtd;
+  final Function(String) atualizaNome;
+  final Function refresh;
+  const UserItemEdit({
+    super.key,
+    required this.name,
+    required this.idEstabelecimento,
+    required this.idItem,
+    required this.qtdItem,
+    required this.validade,
+    required this.lote,
+    required this.atualizaQtd,
+    required this.atualizaNome,
+    required this.refresh,
+  });
+
+  @override
+  State<UserItemEdit> createState() => _UserItemEditState();
+}
+
+class _UserItemEditState extends State<UserItemEdit> {
+  final _addFormKey = GlobalKey<FormState>();
+  final _removeFormKey = GlobalKey<FormState>();
+  final _nameFormKey = GlobalKey<FormState>();
+  final _loteFormKey = GlobalKey<FormState>();
+  final _validadeFormKey = GlobalKey<FormState>();
+  final _add = TextEditingController();
+  final _remove = TextEditingController();
+  final _nome = TextEditingController();
+  final _validade = TextEditingController();
+  final _lote = TextEditingController();
+  final ItemDao _dao = ItemDao();
+
+  addItem() {
+    if (_addFormKey.currentState!.validate()) {
+      widget.atualizaQtd(widget.qtdItem + int.parse(_add.text));
+      _dao
+          .updateQuantity(
+              widget.idItem, (widget.qtdItem + int.parse(_add.text)))
+          .then((id) => Navigator.pop(context));
+    }
+  }
+
+  removeItem() {
+    if (_removeFormKey.currentState!.validate()) {
+      widget.atualizaQtd(widget.qtdItem - int.parse(_remove.text));
+      _dao
+          .updateQuantity(
+              widget.idItem, (widget.qtdItem - int.parse(_remove.text)))
+          .then((id) => Navigator.pop(context));
+    }
+  }
+
+  atualizaNome() {
+    if (_nameFormKey.currentState!.validate()) {
+      widget.atualizaNome(_nome.text.toString());
+      _dao
+          .updateName(widget.idItem, _nome.text)
+          .then((id) => Navigator.pop(context));
+    }
+  }
+
+  atualizaLote() {
+    debugPrint('Chamando a função');
+    widget.refresh();
+    if (_loteFormKey.currentState!.validate()) {
+      _dao
+          .updateLote(widget.idItem, int.parse(_lote.text))
+          .then((id) => Navigator.pop(context));
+    }
+  }
+
+  atualizaValidade() {
+    widget.refresh();
+    if (_validadeFormKey.currentState!.validate()) {
+      _dao
+          .updateValidade(widget.idItem, _validade.text)
+          .then((id) => Navigator.pop(context));
+    }
+  }
+
+  deletaItem() {
+    widget.refresh();
+    _dao.delete(widget.idItem).then((id) => Navigator.pop(context));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _dao.findAllByEstabelecimento(widget.idEstabelecimento);
+    return Scaffold(
+      appBar: MinhaAppBar(
+        title: Text(widget.name,
+            style: const TextStyle(color: Colors.white, fontSize: 36)),
+        elevation: 10,
+      ),
+      body: ListView(
+        children: [
+          Center(
+            child: Text(
+              'Quantidade de ${widget.name}: ${widget.qtdItem}',
+              style: const TextStyle(fontSize: 15),
+            ),
+          ),
+          MyRowForm(
+            formKey: _addFormKey,
+            myController: _add,
+            fieldName: 'add-item'.i18n(),
+            hintText: '',
+            isAddOrRemove: true,
+            operation: addItem,
+          ),
+          MyRowForm(
+            formKey: _removeFormKey,
+            myController: _remove,
+            fieldName: 'remove-item'.i18n(),
+            hintText: '',
+            isAddOrRemove: true,
+            operation: removeItem,
+          ),
+          MyRowForm(
+            formKey: _nameFormKey,
+            myController: _nome,
+            fieldName: 'Nome do item',
+            hintText: widget.name,
+            isAddOrRemove: false,
+            operation: atualizaNome,
+          ),
+          MyRowForm(
+            formKey: _loteFormKey,
+            myController: _lote,
+            fieldName: 'Lote: ${widget.lote}',
+            hintText: '',
+            isAddOrRemove: false,
+            operation: atualizaLote,
+          ),
+          MyRowForm(
+            formKey: _validadeFormKey,
+            myController: _validade,
+            fieldName: 'Validade: ${widget.validade}',
+            hintText: '',
+            isAddOrRemove: false,
+            operation: atualizaValidade,
+          ),
+          DeleteButton(
+            nomeItem: widget.name,
+            deletaItem: deletaItem,
+          ),
+        ],
+      ),
+    );
+  }
+}
